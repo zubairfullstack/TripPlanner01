1af0c63bc4e8d781170d6facf6501a9dc2e6a013
Ashley Heestand <heestand.ashley@gmail.com>
2019-03-11 11:30:43 -0500

    mapbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
 mapboxgl.accessToken = require('./mapboxapikey.js');
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
 
+const markerDomEl = document.createElement('div'); // Create a new, detached DIV
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.641, 41.895]).addTo(map);
